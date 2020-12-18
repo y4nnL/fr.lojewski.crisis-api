8904928560be0a15e1a4706df8a23da028c7af5b
Yann LOJEWKSI <yann.lojewski@gmail.com>
2020-12-18 01:54:49 +0100

    fix: log middleware must use request.originalUrl

--- src/middlewares/logMiddleware.ts
+++ src/middlewares/logMiddleware.ts
@@ -6,11 +6,11 @@ const logLogger = createLogger('log')
 
 export const log: RequestHandler = (request, response, next) => {
   request.startTime = new Date()
-  logLogger.info(`Started ${ request.method } ${ request.url }`)
+  logLogger.info(`Started ${ request.method } ${ request.originalUrl }`)
   response.on('finish', () => {
     if (!response.isErrorHandled) {
       const finishTimeMS = new Date(new Date().getTime() - request.startTime.getTime()).getMilliseconds() / 1000
-      logLogger.info(`Finished ${ request.method } ${ request.url } ${ response.statusCode } in ${ finishTimeMS }s`)
+      logLogger.info(`Finished ${ request.method } ${ request.originalUrl } ${ response.statusCode } in ${ finishTimeMS }s`)
     }
   })
   next()
