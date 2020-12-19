be73e4222c33dbd11b401eeb69c46e60a092cca3
Yann LOJEWKSI <yann.lojewski@gmail.com>
2020-12-19 18:19:20 +0100

    chore: handleNotFound middleware specs

---
+++ src/middlewares/handleNotFoundMiddleware.spec.ts
@@ -0,0 +1,13 @@
+import * as error from '@/types/error'
+import { handleNotFound } from './handleNotFoundMiddleware'
+import { NextFunction, Request, Response } from 'express'
+
+describe('handleNotFound middleware', () => {
+  
+  it('should next an NotFoundAPIError', async () => {
+    let next: NextFunction = <NextFunction>jest.fn()
+    handleNotFound(<Request>{}, <Response>{}, next)
+    expect(next).toHaveBeenCalledWith(new error.NotFoundAPIError())
+  })
+  
+})
