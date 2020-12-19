f189232216af301bd673b184c47947c55f0131fe
Yann LOJEWKSI <yann.lojewski@gmail.com>
2020-12-19 05:55:20 +0100

    fix: Winston has weird behavior concerning error instance

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 import env from '@/utils/env'
 import winston from 'winston'
-import { Logger } from 'winston'
+import { format, Logger } from 'winston'
 
 function typeOf(object: any): string {
   let type = Object.prototype.toString.call(object).slice(8, -1).toLowerCase()
@@ -26,6 +26,28 @@ function getLevelColor(level: string) {
   }
 }
 
+const fixErrorFormat = format(info => {
+  if (<any>info.message instanceof Error) {
+    info.message = <any>{
+      message: (<any>info.message).message,
+      name: (<any>info.message).name,
+      // stack: (<any>info.message).stack,
+      statusCode: (<any>info.message).statusCode,
+    }
+  }
+  if (<any>info instanceof Error) {
+    return Object.assign({
+      message: {
+        message: (<any>info).message,
+        name: (<any>info).name,
+        // stack: (<any>info).stack,
+        statusCode: (<any>info).statusCode,
+      },
+    }, info)
+  }
+  return info
+})
+
 function stringifyMessage(message: any, whiteSpacesCount = 0, startWithNewLine = true): string {
   const type = typeOf(message)
   const whiteSpaces = whiteSpacesCount ? new Array(whiteSpacesCount).join(' ') + ' ' : ''
@@ -40,21 +62,21 @@ function stringifyMessage(message: any, whiteSpacesCount = 0, startWithNewLine =
     return chalk.rgb(152, 118, 170).bgBlack(message)
   } else if (type === 'object' || type === 'class') {
     let prefix = type === 'class' ? `(${ message.constructor.name }) ` : ''
-    for (let key in message) {
-      if (message.hasOwnProperty && message.hasOwnProperty(key)) {
-        return [
-          `${ startWithNewLine ? `\n${ whiteSpaces }` : '' }${ prefix }{\n`,
-          `${ whiteSpacesPlus2 }${ chalk.rgb(152, 118, 170).bgBlack(key) }: `,
-          `${ stringifyMessage(message[key], whiteSpacesCount + 2, false) },\n`,
-          `${ whiteSpaces }}`,
-        ].join('')
-      }
-    }
+    let stringBuilder: string[] = [ `${ startWithNewLine ? `\n${ whiteSpaces }` : '' }${ prefix }{` ]
+    Object.keys(message).forEach((key) => {
+      stringBuilder.push([
+        `${ whiteSpacesPlus2 }${ chalk.rgb(152, 118, 170).bgBlack(key) }: `,
+        `${ stringifyMessage(message[key], whiteSpacesCount + 2, false) },`,
+      ].join(''))
+    })
+    stringBuilder.push(`${ whiteSpaces }}`)
+    return stringBuilder.join('\n')
   } else if (type === 'array') {
-    const stringBuilder = [ `\n${ whiteSpaces }[` ]
-    for (let i = 0, l = message.length; i < l; i++) {
-      stringBuilder.push(`${ whiteSpacesPlus2 }[${ i }] ${ stringifyMessage(message[i], whiteSpacesCount + 2, false) },`)
-    }
+    const stringBuilder: string[] = [ `${ startWithNewLine ? `\n${ whiteSpaces }` : '' }[` ]
+    message.forEach((value: any, i: number) => {
+      stringBuilder.push(
+        `${ whiteSpacesPlus2 }[${ i }] ${ stringifyMessage(message[i], whiteSpacesCount + 2, false) },`)
+    })
     stringBuilder.push(`${ whiteSpaces }]`)
     return stringBuilder.join('\n')
   } else {
@@ -79,14 +101,17 @@ export default function (service: string): Logger {
   } else {
     return winston.createLogger({
       level: 'debug',
-      format: winston.format.printf(debug => {
-        return [
-          new Date().toISOString(),
-          `[${ debug.service }]`,
-          `${ getLevelColor(debug.level) }:`,
-          stringifyMessage(debug.message),
-        ].join(' ')
-      }),
+      format: winston.format.combine(
+        fixErrorFormat(),
+        winston.format.printf(info => {
+          return [
+            new Date().toISOString(),
+            `[${ info.service }]`,
+            `${ getLevelColor(info.level) }:`,
+            stringifyMessage(info.message),
+          ].join(' ')
+        }),
+      ),
       defaultMeta: { service },
       transports: [
         new winston.transports.Console(),
